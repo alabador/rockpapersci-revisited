acdda6ddce5b4dd6216a353cbb5a17a599ef5681
alabador <austinlabador@gmail.com>
2022-04-22 23:11:21 -0700

    Rewrite playRound() to iterate score on click

--- scripts.js
+++ scripts.js
@@ -3,9 +3,15 @@ const rock = document.querySelector("#rock-button");
 const paper = document.querySelector("#paper-button");
 const scissors = document.querySelector("#scissors-button");
 
+const playerResults = document.querySelector('#player-results').innerText;
+const computerResults = document.querySelector('#computer-results').innerText;
+
+let playerScore = 0;
+let compScore = 0;
+
 
 rock.addEventListener('click', () => {
-    playRound('rock'); 
+    playRound('rock');
 });
 paper.addEventListener('click', () => {
     playRound('paper');
@@ -22,68 +28,92 @@ function computerPlay() {
 }
 
 function playRound(playerSelection) {
-    let lowercasePlayer = playerSelection.toLowerCase().toString();
-    let lowercaseComp = computerPlay().toLowerCase().toString();
-    
-    
-    if (lowercasePlayer == "rock" && lowercaseComp == "scissors"){
-        return("You Win! Rock beats Scissors!");
+    playerSelection = playerSelection.toLowerCase();
+    let compSelection = computerPlay().toLowerCase();
+    if (playerSelection == "rock" && compSelection == "scissors"){
+        console.log('test');
+        return playerScore++;
     }
-    else if (lowercasePlayer == "rock" && lowercaseComp == "paper"){
-        return("You Lose! Rock loses to Paper!")
+    else if (playerSelection == "rock" && compSelection == "paper"){
+        console.log('test');
+        return compScore++;
     }
-    else if (lowercasePlayer == "rock" && lowercaseComp == "rock"){
-        return("It's a tie! Rock ties with Rock.")
+    else if (playerSelection == "rock" && compSelection == "rock"){
+        console.log('test');
+        return "It's a tie! Rock ties with Rock.";
     }
-    else if (lowercasePlayer == "paper" && lowercaseComp == "rock"){
-        return("You Win! Paper beats Rock!")
+    else if (playerSelection == "paper" && compSelection == "rock"){
+        return playerScore++;
     }
-    else if (lowercasePlayer == "paper" && lowercaseComp == "scissors"){
-        return("You Lose! Paper loses to Scissors!")
+    else if (playerSelection == "paper" && compSelection == "scissors"){
+        return compScore++;
     }
-    else if (lowercasePlayer == "paper" && lowercaseComp == "paper"){
-        return("It's a tie! Paper ties with Paper.")
+    else if (playerSelection == "paper" && compSelection == "paper"){
+        return "It's a tie! Paper ties with Paper.";
     }
-    else if (lowercasePlayer == "scissors" && lowercaseComp == "paper"){
-        return("You Win! Scissors beats Paper!")
+    else if (playerSelection == "scissors" && compSelection == "paper"){
+        return playerScore++;
     }
-    else if (lowercasePlayer == "scissors" && lowercaseComp == "rock"){
-        return("You Lose! Scissors loses to Rock!")
+    else if (playerSelection == "scissors" && compSelection == "rock"){
+        return compScore++;
     }
-    else if (lowercasePlayer == "scissors" && lowercaseComp == "scissors"){
-        return("It's a tie! Scissors ties with Scissors.")
+    else if (playerSelection == "scissors" && compSelection == "scissors"){
+        return "It's a tie! Scissors ties with Scissors.";
+    }
+    // console.log(`Player score is ${playerScore}`);
+    // console.log (`Computer score is ${compScore}`);
+}
+
+
+
+
+// function checkSelection(selection) {
+//     if (selection === 'rock') {
+//         return 
+//     }
+//     else if (selection === 'paper') {
+
+//     }
+//     else if (selection === 'scissors') {
+
+//     }
+// }
+
+function win() {
+    if (playerScore == 5) {
+        return 'Player wins.'
+    }
+    else if (compScore == 5) {
+        return 'Computer wins.'
     }
 }
 
-// function game() {
-//     let playerScore = 0;
-//     let compScore = 0;
-
-     
-//     playerInput = window.prompt("Rock, Paper, or Scissors?");
-//     // playerInput = chooseSelection();
-//     let result = playRound(playerInput, computerPlay());
-
-//     if ((result.includes("Win"))) {
-//         ++playerScore;
-//         console.log("win");
-//     }
-
-//     else if ((result.includes("Lose"))) {
-//         ++compScore;
-//         console.log("loss");
-//     }
-
-//     console.log(result + " Player Score:" + playerScore + "  Computer Score:" + compScore);
-
-
-//     if (playerScore > compScore) {
-//         console.log("Player wins!");
-//     }
-//     else if (playerScore < compScore) {
-//         console.log("Computer wins!");
-//     }
-//     else if (playerScore == compScore) {
-//         console.log("It's a tie.");
-//     }
-// }
+function winCons(playerSelection, compSelection) {
+    if (playerSelection == "rock" && compSelection == "scissors"){
+        return playerScore++;
+    }
+    else if (playerSelection == "rock" && compSelection == "paper"){
+        return compScore++;
+    }
+    else if (playerSelection == "rock" && compSelection == "rock"){
+        return "It's a tie! Rock ties with Rock.";
+    }
+    else if (playerSelection == "paper" && compSelection == "rock"){
+        return playerScore++;
+    }
+    else if (playerSelection == "paper" && compSelection == "scissors"){
+        return compScore++;
+    }
+    else if (playerSelection == "paper" && compSelection == "paper"){
+        return "It's a tie! Paper ties with Paper.";
+    }
+    else if (playerSelection == "scissors" && compSelection == "paper"){
+        return playerScore++;
+    }
+    else if (playerSelection == "scissors" && compSelection == "rock"){
+        return compScore++;
+    }
+    else if (playerSelection == "scissors" && compSelection == "scissors"){
+        return "It's a tie! Scissors ties with Scissors.";
+    }
+}
